39778ecab9e642a54fd3a5b688f6bd2de4f7a71a
Yannick Pflanzer <yp1995@outlook.com>
2016-10-28 16:16:32 +0200

    + Added some files for the runtime library

---
+++ .dir-locals.el
@@ -0,0 +1,28 @@
+((c-mode
+  (indent-tabs-mode . t)
+  (tab-width . 4)
+  (c-basic-offset . 4)
+  (c-file-style . "bsd"))
+
+ (c++-mode
+  (indent-tabs-mode . t)
+  (tab-width . 4)
+  (c-basic-offset . 4)
+  (c-file-style . "bsd"))
+
+ (nil . ((eval .
+			   (progn
+
+				 (setq project-directory (file-name-directory
+										  (let ((d (dir-locals-find-file ".")))
+											(if (stringp d) d (car d)))))
+
+				 
+				 (setq cmake-ide-project-dir project-directory)
+				 (setq cmake-ide-build-dir (concat project-directory "build"))
+				 (setq cmake-compile-command "make -j8")
+				 (setenv "MAKEFLAGS" "-j5")
+				 (setq-default indent-tabs-mode t)))))) 
+
+
+ 
