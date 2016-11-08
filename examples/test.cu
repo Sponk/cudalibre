c4700048bc6ecb5cad7e5c2409b979175d958021
Yannick Pflanzer <yp1995@outlook.com>
2016-11-08 16:26:33 +0100

    * Print device info in example

--- examples/test.cu
+++ examples/test.cu
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <cuda_runtime.h>
+#include <iostream>
 
 __global__ void testkernel(__device__ float* a, __device__ float* b, __device__ float* c)
 {
@@ -7,9 +8,35 @@ __global__ void testkernel(__device__ float* a, __device__ float* b, __device__ 
 	c[id] = a[id] + b[id];
 }
 
+void print_cuda_devices()
+{
+	int num = 0;
+  	struct cudaDeviceProp devprop;
+
+  	cudaGetDeviceCount(&num);
+  	if(!num)
+    {
+    	std::cout << "No CUDA devices found." << std::endl;
+      	return;
+    }
+
+  	for(int i = 0; i < num; i++)
+	{
+  		cudaGetDeviceProperties(&devprop, i);
+      	std::cout << "Found CUDA device: " << devprop.name << std::endl;
+		std::cout << "\tCompute Capability: " << devprop.major << "." << devprop.minor << std::endl;
+		std::cout << "\tMultiprocessor Count: " << devprop.multiProcessorCount << std::endl;
+		std::cout << "\tClock Rate: " << static_cast<float>(devprop.clockRate)/1024.0f/1024.0f << "Ghz" << std::endl;
+		std::cout << "\tTotal Global: " << devprop.totalGlobalMem/1024 << "MB" << std::endl;
+		std::cout << "\tTotal L2 Cache: " << devprop.l2CacheSize << "KB" << std::endl;
+	}
+}
+
 #define TESTSIZE 32
 int main(int argc, char* argv[])
 {
+	print_cuda_devices();
+
 	float *da, *db, *dc, *a, *b, *c;
 
 	size_t pitch;
