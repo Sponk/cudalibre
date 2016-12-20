d02ba5e4c52c6c402a02effe97059b8323a0e567
Yannick Pflanzer <yp1995@outlook.com>
2016-12-20 15:08:40 +0100

    + Added missing file

---
+++ examples/VectorPointerStructTest.cu
@@ -0,0 +1,67 @@
+#include <cuda_runtime.h>
+#include <iostream>
+#include <cmath>
+#include "common.h"
+
+struct Vector
+{
+	float* data;
+	unsigned int size;
+};
+
+__global__ void add(struct Vector a, struct Vector b, struct Vector c)
+{
+	unsigned int id = threadIdx.x;
+	c.data[id] = a.data[id] + b.data[id];
+}
+
+#define TESTSIZE 16
+int main(int argc, char* argv[])
+{
+
+	Vector dav, dbv, dcv;
+	float *a, *b, *c;
+
+	size_t pitch;
+
+	CUDA_CHECK(cudaMalloc((void**) &dav.data, sizeof(float) * TESTSIZE));
+	CUDA_CHECK(cudaMalloc((void**) &dbv.data, sizeof(float) * TESTSIZE));
+	CUDA_CHECK(cudaMalloc((void**) &dcv.data, sizeof(float) * TESTSIZE));
+
+   	a = new float[TESTSIZE];
+   	b = new float[TESTSIZE];
+   	c = new float[TESTSIZE];
+
+   	for(int i = 0; i < TESTSIZE; i++) a[i] = i;
+	for(int i = 0; i < TESTSIZE; i++) b[i] = TESTSIZE - i;
+
+	CUDA_CHECK(cudaMemcpy(dav.data, a, sizeof(float) * TESTSIZE, cudaMemcpyHostToDevice));
+	CUDA_CHECK(cudaMemcpy(dbv.data, b, sizeof(float) * TESTSIZE, cudaMemcpyHostToDevice));
+
+	dav.size = TESTSIZE;
+
+	// Test addition kernel
+	add<<<1, TESTSIZE>>>(dav, dbv, dcv);
+	CUDA_CHECK_LAST;
+
+	CUDA_CHECK(cudaMemcpy2D(dcv.data, pitch, c, sizeof(float), sizeof(float), TESTSIZE, cudaMemcpyDeviceToHost));
+
+	// Check expected result
+	int retval = 0;
+	for(int i = 0; i < TESTSIZE; i++)
+		if(fabs(c[i] - (a[i] + b[i])) > 0.0001)
+		{
+			std::cerr << "Error: Result is not as expected!" << std::endl;
+			retval = 1;
+		}
+
+	CUDA_CHECK(cudaFree(dav.data));
+	CUDA_CHECK(cudaFree(dbv.data));
+	CUDA_CHECK(cudaFree(dcv.data));
+
+	delete[] a;
+	delete[] b;
+	delete[] c;
+
+	return retval;
+}
