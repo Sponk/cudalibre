7836fedffd292af39f037989c7979728a5f8bdb2
Yannick Pflanzer <yp1995@outlook.com>
2016-12-21 15:08:38 +0100

    + Added culccd compiler driver to emulate nvcc

---
+++ culccd/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cstring>
+#include <stdlib.h>
+#include <sstream>
+
+int main(int argc, char** argv)
+{
+	std::vector<std::string> gccArgs;
+	std::string source;
+
+	if(argc <= 1)
+	{
+		return system("g++");
+	}
+
+	for(int i = 1; i < argc; i++)
+	{
+		if(strstr(argv[i], "-gencode")
+			|| strstr(argv[i], "-X"))
+			continue;
+
+		gccArgs.push_back(argv[i]);
+	}
+
+	std::stringstream args;
+	if(gccArgs.empty())
+	{
+		return 1;
+	}
+
+	source = gccArgs.back();
+	gccArgs.pop_back();
+
+	args << "g++ -I/usr/include/cudalibre -lOpenCL -Wl,-rpath=. -lclruntime";
+	for(auto& s : gccArgs)
+		args << " " << s;
+
+	args << " " << source << ".cpp";
+
+	std::string culccArgs = "culcc -s ";
+	culccArgs += source + " -o " + source + ".cpp";
+
+	if(system(culccArgs.c_str()))
+	{
+		std::cerr << "Could not preprocess CUDA file!" << std::endl;
+		return 1;
+	}
+
+	return system(args.str().c_str());
+}
