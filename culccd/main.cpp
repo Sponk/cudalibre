02d32bc8dcb9b3c970f456e5d1bada3d607a5fd3
Yannick Pflanzer <yp1995@outlook.com>
2016-12-26 10:54:46 +0100

    * Some compatibility changes in the compiler driver

--- culccd/main.cpp
+++ culccd/main.cpp
@@ -9,6 +9,7 @@ int main(int argc, char** argv)
 {
 	std::vector<std::string> gccArgs;
 	std::string source;
+	std::string outfile;
 
 	if(argc <= 1)
 	{
@@ -18,8 +19,20 @@ int main(int argc, char** argv)
 	for(int i = 1; i < argc; i++)
 	{
 		if(strstr(argv[i], "-gencode")
-			|| strstr(argv[i], "-X"))
+			|| strstr(argv[i], "-X")
+			|| strstr(argv[i], "-arch"))
 			continue;
+
+		if(!strcmp("-o", argv[i]) && i < argc - 1)
+		{
+			outfile = argv[++i];
+			continue;
+		}
+		else if(!strcmp("-isystem", argv[i]))
+		{
+			gccArgs.push_back("-I");
+			continue;
+		}
 
 		gccArgs.push_back(argv[i]);
 	}
@@ -33,16 +46,15 @@ int main(int argc, char** argv)
 	source = gccArgs.back();
 	gccArgs.pop_back();
 
-	args << "g++ -I/usr/include/cudalibre -lOpenCL -Wl,-rpath=. -lclruntime";
 	for(auto& s : gccArgs)
 		args << " " << s;
+
+	args << " -I/usr/include -I/usr/include/cudalibre -lOpenCL -Wl,-rpath=. -lclruntime";
 
 	std::string culccArgs = "culcc ";
 
 	// produces something like "culcc <source> -o <out> -- <some args>"
 	culccArgs += source + " -o " + source + ".cpp -- " + args.str();
-
-	// std::cout << culccArgs << std::endl;
 
 	if(system(culccArgs.c_str()))
 	{
@@ -51,6 +63,10 @@ int main(int argc, char** argv)
 	}
 
 	// Add source file here so it does not interfere with the culccArgs earlier
+
+	if(!outfile.empty())
+		args << " -o " << outfile;
+
 	args << " " << source << ".cpp";
-	return system(args.str().c_str());
+	return system(("g++ " + args.str()).c_str());
 }
