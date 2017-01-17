5b81341a9189a79f9d774d2edde848a162d0686a
Yannick Pflanzer <yp1995@outlook.com>
2017-01-17 09:44:19 +0100

    + Added tests for cudastd + Added some stubs for CUDA math builtins

--- cudastd/math.cuh
+++ cudastd/math.cuh
@@ -47,7 +47,7 @@ extern dim3 blockDim;
 #ifndef __local
 #define __local __attribute__((annotate("local")))
 #endif
-#endif
+#endif // __CUDA_LIBRE_TRANSLATION_PHASE__
 
 // Some stuff used in the compiler to prevent errors while transforming code
 // @note These won't appear in the OpenCL code since they are defined as macros. THIS IS NECESSARY! DON'T CHANGE!
@@ -69,21 +69,103 @@ __DEFINE_VECSTRUCT4(unsigned int, uint4)
 
 #ifdef __CUDACC__
 
+/// Used, when its name has to be exactly the same
 #define _CL_BUILTIN_ __attribute__((annotate("builtin")))
 
+/// Used, when the name has a CUDA typical trailing 'f' that needs to be cut off.
+#define _CL_BUILTINF_ __attribute__((annotate("builtinf")))
+
+/**
+ * @defgroup cudastd CUDA Device Standard Library
+ * @addtogroup cudastd
+ *  @{
+ */
+ 
+/// @todo Implementation
 _CL_BUILTIN_ __device__ extern float abs(float);
 _CL_BUILTIN_ __device__ extern float fabs(float);
-_CL_BUILTIN_ __device__ extern float sqrtf(float);
-_CL_BUILTIN_ __device__ extern float floorf(float);
+_CL_BUILTINF_ __device__ extern float sqrtf(float);
+_CL_BUILTINF_ __device__ extern float floorf(float);
 
-_CL_BUILTIN_ __device__ extern float fminf(float a, float b);
-_CL_BUILTIN_ __device__ extern float fmaxf(float a, float b);
+_CL_BUILTINF_ __device__ extern float fminf(float a, float b);
+_CL_BUILTINF_ __device__ extern float fmaxf(float a, float b);
 _CL_BUILTIN_ __device__ extern int max(int a, int b);
 _CL_BUILTIN_ __device__ extern int min(int a, int b);
-_CL_BUILTIN_ __device__ extern float rsqrtf(float x);
-_CL_BUILTIN_ __device__ extern float fmodf(float a, float b);
+_CL_BUILTINF_ __device__ extern float rsqrtf(float x);
+_CL_BUILTINF_ __device__ extern float fmodf(float a, float b);
+           
+_CL_BUILTINF_ __device__ extern float acosf(float x);
+_CL_BUILTINF_ __device__ extern float acoshf(float x);
+_CL_BUILTINF_ __device__ extern float asinf(float x);
+_CL_BUILTINF_ __device__ extern float asinhf(float x);
+_CL_BUILTINF_ __device__ extern float atanf(float x);
+_CL_BUILTINF_ __device__ extern float cbrtf(float x);
+_CL_BUILTINF_ __device__ extern float ceilf(float x);
+_CL_BUILTINF_ __device__ extern float copysignf(float x);
+_CL_BUILTINF_ __device__ extern float cosf(float x);
+_CL_BUILTINF_ __device__ extern float coshf(float x);
+
+/**
+ * @brief \f$cospif(x) := cos(\pi * x)\f$
+ * @param x Input.
+ * @return \f$cos(\pi * x)\f$
+ */
+_CL_BUILTINF_ __device__ extern float cospif(float x);
+
+_CL_BUILTINF_ __device__ extern float cyl_besseli0f(float x);
+_CL_BUILTINF_ __device__ extern float cyl_bessel_i1f(float x);
+_CL_BUILTINF_ __device__ extern float erfcinvf(float x);
+_CL_BUILTINF_ __device__ extern float erfcf(float x);
+_CL_BUILTINF_ __device__ extern float erfcxf(float x);
+_CL_BUILTINF_ __device__ extern float erff(float x);
+_CL_BUILTINF_ __device__ extern float erfinvf(float x);
+_CL_BUILTINF_ __device__ extern float exp10f(float x);
+_CL_BUILTINF_ __device__ extern float exp2f(float x);
+_CL_BUILTINF_ __device__ extern float expm1f(float x);
+_CL_BUILTINF_ __device__ extern float fabsf(float x);
+_CL_BUILTINF_ __device__ extern float floorf(float x);
+_CL_BUILTINF_ __device__ extern float ilogbf(float x);
+_CL_BUILTINF_ __device__ extern float isfinite(float x);
+_CL_BUILTINF_ __device__ extern float isinf(float x);
+_CL_BUILTINF_ __device__ extern float isnan(float x);
+_CL_BUILTINF_ __device__ extern float j0f(float x);
+_CL_BUILTINF_ __device__ extern float j1f(float x);
+_CL_BUILTINF_ __device__ extern float lgammaf(float x);
+_CL_BUILTINF_ __device__ extern long long int llrintf(float x);
+_CL_BUILTINF_ __device__ extern long long int llroundf(float x);
+_CL_BUILTINF_ __device__ extern float log10f(float x);
+_CL_BUILTINF_ __device__ extern float log1pf(float x);
+_CL_BUILTINF_ __device__ extern float log2f(float x);
+_CL_BUILTINF_ __device__ extern float logbf(float x);
+_CL_BUILTINF_ __device__ extern float logf(float x);
+_CL_BUILTINF_ __device__ extern float nearbyintf(float x);
+_CL_BUILTINF_ __device__ extern float normcdff(float x);
+_CL_BUILTINF_ __device__ extern float normcdfinvf(float x);
+_CL_BUILTINF_ __device__ extern float normf(float x);
+_CL_BUILTINF_ __device__ extern float powf(float x);
+_CL_BUILTINF_ __device__ extern float rcbrtf(float x);
+_CL_BUILTINF_ __device__ extern float rintf(float x);
+_CL_BUILTINF_ __device__ extern float roundf(float x);
+_CL_BUILTINF_ __device__ extern float rsqrtf(float x);
+_CL_BUILTINF_ __device__ extern float sinf(float x);
+_CL_BUILTINF_ __device__ extern float sinhf(float x);
+_CL_BUILTINF_ __device__ extern float sinpif(float x);
+_CL_BUILTINF_ __device__ extern float sqrtf(float x);
+_CL_BUILTINF_ __device__ extern float tanf(float x);
+_CL_BUILTINF_ __device__ extern float tanhf(float x);
+_CL_BUILTINF_ __device__ extern float tgammaf(float x);
+_CL_BUILTINF_ __device__ extern float truncf(float x);
+_CL_BUILTINF_ __device__ extern float y0f(float x);
+_CL_BUILTINF_ __device__ extern float y1f(float x);
+
+_CL_BUILTINF_ __device__ extern float ynf(int n, float x);
+_CL_BUILTINF_ __device__ extern float jnf(int n, float x);
+
 
 #undef _CL_BUILTIN_
+#undef __DEFINE_VECSTRUCT2
+#undef __DEFINE_VECSTRUCT3
+#undef __DEFINE_VECSTRUCT4
 
 #else
 #include <math.h>
@@ -102,3 +184,7 @@ __host__ __device__ int4 make_int4(int x, int y, int z, int w);
 __host__ __device__ uint2 make_uint2(unsigned int x, unsigned int y);
 __host__ __device__ uint3 make_uint3(unsigned int x, unsigned int y, unsigned int z);
 __host__ __device__ uint4 make_uint4(unsigned int x, unsigned int y, unsigned int z, unsigned int w);
+
+/**
+ * @}
+ */
